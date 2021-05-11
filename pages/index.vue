ec3d482734ead2f354a7429e5944167f773bcfb3
Pierre Gabriel <pierregabriel@me.com>
2021-05-11 15:19:52 +0200

    Use static data for paragraphs on `Index.vue`

--- pages/index.vue
+++ pages/index.vue
@@ -57,6 +57,55 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from '~/static/ScrollTrigger.min.js';
 gsap.registerPlugin(ScrollTrigger);
 
+const contactParagraphs = Object.freeze([
+  {
+    title: 'Missions Front-end',
+    description: `Vous êtes une <strong>Entreprise</strong>, une <strong>Start-up</strong>,
+    une <strong>Agence Web</strong> ou un(e) <strong>Développeur(euse)</strong> et vous avez besoin d’une
+    développeuse Front-end ?<br />
+    Je suis disponible pour des <strong>missions</strong> de
+    courte ou longue durée, et à l'écoute des propositions de postes dans ce domaine.`,
+  },
+]);
+
+const aboutParagraphs = Object.freeze([
+  {
+    title: 'Expertise',
+    description: `Je suis spécialisée en développement
+    <strong>Front-end</strong> avec une appétence
+    pour <strong>l'animation</strong> et le <strong>Javascript</strong>, depuis ma
+    certification au Wagon à Nantes en mars 2020.`,
+    svg: 'icon-mesure',
+  },
+  {
+    title: 'Expérience',
+    description: `J'ai réalisé des sites WordPress pendant 2 ans.
+    Cela m'a permis de m'intéresser aux notions de
+      <strong>performances, accessibilité, sécurité et SEO</strong> :
+    Un ensemble de bonnes pratiques à implémenter dès la construction d'un site, indispensable à
+    sa longévité et une base solide à un référencement naturel de qualité.`,
+    svg: 'icon-book',
+  },
+  {
+    title: 'Parcours',
+    description: `Je suis aussi SybR,
+    <strong
+      ><a
+        class="body-link"
+        href="https://sybilrondeau.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        >photographe</a
+      ></strong
+    >
+    documentaire de famille depuis plus de 10 ans.<br />
+    Ce métier était ma première reconversion, après 12 ans en
+    tant que <b>manipulatrice en radiologie</b> médicale aux urgences
+    (et de nuit !).`,
+    svg: 'icon-camera',
+  },
+]);
+
 export default {
   components: {
     TheHeader,
@@ -66,56 +115,9 @@ export default {
     BaseCta,
     IconBase,
   },
-  data() {
-    return {
-      aboutParagraphs: [
-        {
-          title: 'Expertise',
-          description: `Je suis spécialisée en développement
-          <strong>Front-end</strong> avec une appétence
-          pour <strong>l'animation</strong> et le <strong>Javascript</strong>, depuis ma
-          certification au Wagon à Nantes en mars 2020.`,
-          svg: 'icon-mesure',
-        },
-        {
-          title: 'Expérience',
-          description: `J'ai réalisé des sites WordPress pendant 2 ans.
-          Cela m'a permis de m'intéresser aux notions de
-           <strong>performances, accessibilité, sécurité et SEO</strong> : 
-          Un ensemble de bonnes pratiques à implémenter dès la construction d'un site, indispensable à 
-          sa longévité et une base solide à un référencement naturel de qualité.`,
-          svg: 'icon-book',
-        },
-        {
-          title: 'Parcours',
-          description: `Je suis aussi SybR,
-          <strong
-            ><a
-              class="body-link"
-              href="https://sybilrondeau.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              >photographe</a
-            ></strong
-          >
-          documentaire de famille depuis plus de 10 ans.<br />
-          Ce métier était ma première reconversion, après 12 ans en
-          tant que <b>manipulatrice en radiologie</b> médicale aux urgences
-          (et de nuit !).`,
-          svg: 'icon-camera',
-        },
-      ],
-      contactParagraphs: [
-        {
-          title: 'Missions Front-end',
-          description: `Vous êtes une <strong>Entreprise</strong>, une <strong>Start-up</strong>, 
-          une <strong>Agence Web</strong> ou un(e) <strong>Développeur(euse)</strong> et vous avez besoin d’une
-          développeuse Front-end ?<br />
-          Je suis disponible pour des <strong>missions</strong> de
-          courte ou longue durée, et à l'écoute des propositions de postes dans ce domaine.`,
-        },
-      ],
-    };
+  created() {
+    this.contactParagraphs = contactParagraphs;
+    this.aboutParagraphs = aboutParagraphs;
   },
   mounted() {
     function animSections(sectionItems, trigger, stagger) {
