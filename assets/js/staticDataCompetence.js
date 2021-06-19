823b3ccf687e95772c549849390815c4ca50013b
Sybil Rondeau <contact@sybilrondeau.com>
2021-06-19 10:25:18 +0200

    adjust skills

--- assets/js/staticDataCompetence.js
+++ assets/js/staticDataCompetence.js
@@ -1,28 +1,8 @@
 export const competences = Object.freeze([
   {
-    name: 'HTML',
-    viewBox: '0 0 256 361',
-    svg: 'icon-html',
-  },
-  {
-    name: 'CSS',
-    viewBox: '0 0 256 361',
-    svg: 'icon-css',
-  },
-  {
     name: 'Sass',
     viewBox: '0 0 256 192',
     svg: 'icon-sass',
-  },
-  {
-    name: 'Javascript',
-    viewBox: '0 0 256 256',
-    svg: 'icon-js',
-  },
-  {
-    name: 'Ruby on Rails',
-    viewBox: '0 0 512 180',
-    svg: 'icon-rails',
   },
   {
     name: 'Vue',
