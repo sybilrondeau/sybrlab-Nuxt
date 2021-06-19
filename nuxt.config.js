cca7303e567062b2f46401eadeb05af18791cfa3
Sybil Rondeau <contact@sybilrondeau.com>
2021-06-19 11:25:16 +0200

    change path to meta images

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
       },
       {
         property: 'og:image',
-        content: '@/static/sybrlab-1200.jpg',
+        content: '/sybrlab-1200.jpg',
       },
       {
         property: 'og:image:type',
@@ -47,7 +47,7 @@ export default {
       },
       {
         property: 'twitter:image',
-        content: '@/static/sybrlab-150.jpg',
+        content: '/sybrlab-150.jpg',
       },
     ],
     link: [
